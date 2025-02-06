cf3fc5ddf658f38d197835b5a055aa8fbd213256
oktntko <oktntko@gmail.com>
2025-02-07 00:41:37 +0900

    Updates

--- assets/game-Dwgk_CzQ.js
+++
@@ -1,1 +0,0 @@
-import{d as a,w as s,c as n,a as r,p as c,r as _,o as p}from"./index-BPmW-NV7.js";const i={class:"min-h-dvh bg-gradient-to-b from-gray-400 to-white"},u=a({__name:"game",async setup(m){let e,t;return[e,t]=s(()=>c()),await e,t(),(d,l)=>{const o=_("RouterView");return p(),n("div",i,[r(o)])}}});export{u as default};
